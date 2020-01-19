57680c833c83b54a3a6982d5caaaa553cc84f616
BuildTools <unconfigured@null.spigotmc.org>
2020-01-19 16:48:46 -0700

    Used json

--- InstaMeme/index.js
+++ InstaMeme/index.js
@@ -4,8 +4,8 @@ const puppy = require('random-puppy');
 const request = require('request');
 const keys = require('./keys')
 
-var arr = ["When WWIII releases 2x XP weekend", "For When the Intensity Isn't Intense Enough", "The Most Accurate Way to Cry", "This Can't Be Right", "Closed-Caption Bullying", "Firefighters Are the Bravest Americans", "The Only Way the Deaf Can Understand Tennis", "LOL this reminds me of Donald Trump", "Or 'The Sounds of Cannibalistic Implications'", "Like a Real Man", "Doctor Who Villain Emulates Not Dogs", "D'Awww", "The Darkest, Most Violent Vegetable-Based Kids Show", "Stickin It to the Man", "Easily the Most Traumatizing Kind of Stealing", "[Insert Monster Mash Joke]", "This Is Called a Snart", "Because She Is That Much Worse Than Oliver", "This Is Now My Desktop Background", "Stupid Baby, Serves Him Right", "Your Own Recipe Right in the Comfort of Your Very Own Home!"];
-var arr2 = ["http://via.placeholder.com/640x360", "https://www.fillmurray.com/640/360", "https://loremflickr.com/640/360", "https://baconmockup.com/640/360", "https://placekitten.com/640/360", "https://placebeard.it/640x360", "http://lorempixel.com/640/360", "https://www.placecage.com/640/360", "http://placeimg.com/640/360/any", "https://placebear.com/640/360", "https://picsum.photos/640/360"];
+var arr = require('./MemeText.json');
+var arr2 = require('./MemeImageLinks.json');
     function random(mn, mx) {
         return Math.random() * (mx - mn) + mn;
 }
@@ -17,7 +17,7 @@ var download = function (uri, filename, callback) {
 
             request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
         });
-};  
+};
 let T = new Twit(keys.keys)
 var b64content;
 function getMeme() {
@@ -41,14 +41,14 @@ getLink()
 
 function getText() {
 
-    Thetext = arr[Math.floor(random(1, arr.length))-1];
+    Thetext = arr.text[Math.floor(random(1, arr.text.length))-1];
     //////console.log(Thetext)
 
 }
 
 function getLink() {
 
-    Thelink = arr2[Math.floor(random(1, arr2.length))-1];
+    Thelink = arr2.links[Math.floor(random(1, arr2.links.length))-1];
     //////console.log(Thelink);
 
 }
@@ -61,7 +61,7 @@ T.post('media/upload', {
     // now we can assign alt text to the media, for use by screen readers and
     // other text-based presentations and interpreters
     var mediaIdStr = data.media_id_string
-    var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+    var altText = "A meme"
     var meta_params = {
         media_id: mediaIdStr,
         alt_text: {
