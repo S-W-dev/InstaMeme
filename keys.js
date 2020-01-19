f3972e71d3a94eaf0736bdf0ccaff2c70c606d9e
Walter Shewmake <25shewmwalj@washk12.org>
2020-01-18 23:28:20 -0700

    updated something

--- keys.js
+++ keys.js
@@ -1,3 +1,8 @@
 module.exports {
-    
+    keys = {
+        consumer_key: "vZPGdk5EkRkuDeGG7uqBceESi",
+        consumer_secret: "fwCSHh03iJmlkNsNWGAlOSgrapL3SfGNrzh8RBISNTY9wuci5c",
+        access_token: "1218731766480945154-OO1Qf4EGQeXZLQL9n6QhGV7DX1keCb",
+        access_token_secret: "eO6BS9aMofHlU8JmBAMg0MRUmfpoKBRxfzhxcgKK9dYSE"
+    };
 }
