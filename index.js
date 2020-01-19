e0d6c8a537eb003541c3e06ca68810d32bce35e9
Walter Shewmake <25shewmwalj@washk12.org>
2020-01-19 11:33:08 -0700

    fixed a bug

--- index.js
+++ index.js
@@ -1,86 +1,66 @@
-const Twit = require('twit');
-const fs = require('fs');
-const puppy = require('random-puppy');
-const request = require('request');
-const keys = require('./keys')
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
 
-var arr = ["When WWIII releases 2x XP weekend", "For When the Intensity Isn't Intense Enough", "The Most Accurate Way to Cry", "This Can't Be Right", "Closed-Caption Bullying", "Firefighters Are the Bravest Americans", "The Only Way the Deaf Can Understand Tennis", "LOL this reminds me of Donald Trump", "Or 'The Sounds of Cannibalistic Implications'", "Like a Real Man", "Doctor Who Villain Emulates Not Dogs", "D'Awww", "The Darkest, Most Violent Vegetable-Based Kids Show", "Stickin It to the Man", "Easily the Most Traumatizing Kind of Stealing", "[Insert Monster Mash Joke]", "This Is Called a Snart", "Because She Is That Much Worse Than Oliver", "This Is Now My Desktop Background", "Stupid Baby, Serves Him Right", "Your Own Recipe Right in the Comfort of Your Very Own Home!"];
-var arr2 = ["http://via.placeholder.com/640x360", "https://www.fillmurray.com/640/360", "https://loremflickr.com/640/360", "https://baconmockup.com/640/360", "https://placekitten.com/640/360", "https://placebeard.it/640x360", "http://lorempixel.com/640/360", "https://www.placecage.com/640/360", "http://placeimg.com/640/360/any", "https://placebear.com/640/360", "https://www.stevensegallery.com/640/360", "https://picsum.photos/640/360"];
-function random(mn, mx) {
-    return Math.random() * (mx - mn) + mn;
-}
-var thelink;
-var download = function (uri, filename, callback) {
-        request.head(uri, function (err, res, body) {
-            console.log('content-type:', res.headers['content-type']);
-            console.log('content-length:', res.headers['content-length']);
+var exec = require('child_process').exec;
 
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-        });
-};  
-let T = new Twit(keys.keys)
-var b64content;
-function getMeme() {
-    puppy().then(url => {
-        download(Thelink, 'google.png', function () {
-            console.log('done downloading');
-            b64content = fs.readFileSync('./google.png', {
-                encoding: 'base64'
-            })
-            post();
-        });
+function execute(command, callback) {
+    exec(command, function (error, stdout, stderr) {
+        callback(stdout);
+    });
+};
 
-    })
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
 
-}
-
-var Thetext = "skrt";
-getMeme();
-getText();
-getLink()
-
-function getText() {
-
-    Thetext = arr[Math.floor(random(1, arr.length))-1];
-    console.log(Thetext)
-
-}
-
-function getLink() {
-
-    Thelink = arr2[Math.floor(random(1, arr2.length))-1];
-    console.log(Thelink);
-
-}
-
-function post() {
-// first we must post the media to Twitter
-T.post('media/upload', {
-    media_data: b64content
-}, function (err, data, response) {
-    // now we can assign alt text to the media, for use by screen readers and
-    // other text-based presentations and interpreters
-    var mediaIdStr = data.media_id_string
-    var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-    var meta_params = {
-        media_id: mediaIdStr,
-        alt_text: {
-            text: altText
+        args = args.splice(1);
+        switch (cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            case 'meme':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Generating meme... Visit this link to see a bunch of other memes: https://twitter.com/InstaMeme7'
+                });
+                var final_text;
+                execute("node ./InstaMeme/index.js", (output) => {
+                    console.log(output);
+                    final_text = output;
+                });
+                setTimeout(() => {
+                    console.log('sending message: ' + final_text);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: final_text
+                    });
+                }, 5000)
+                break;
+                // Just add any case commands if you want to..
         }
     }
-
-    T.post('media/metadata/create', meta_params, function (err, data, response) {
-        if (!err) {
-            // now we can reference the media and post a tweet (media will attach to the tweet)
-            var params = {
-                status: Thetext,
-                media_ids: [mediaIdStr]
-            }
-
-            T.post('statuses/update', params, function (err, data, response) {
-                console.log(data)
-            })
-        }
-    })
-})
-}
+});
