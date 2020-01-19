a792ea3592c659ac76fbbbdc92f949663d316974
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 18:36:54 -0700

    Stuff Happened
    I added a lot of STOOF

--- InstaMeme/Program.cs
+++ InstaMeme/Program.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InstaSharp;
+using InstaSharper;
+using InstaSharper.API;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Logger;
 
 namespace InstaMeme {
     class Program {
+
+        #region Hidden
+        private const string username = "InstaMeme1O1";
+        private const string password = "Snakeman!30";
+        #endregion
+        private static UserSessionData user;
+        private static IInstaApi api;
+
         static void Main(string[] args) {
+            new discord().MainAsync().GetAwaiter().GetResult();
+            user = new UserSessionData();
+            user.UserName = username;
+            user.Password = password;
+            Login();
+            Console.Read();
+        }
+
+        public static async void Login() {
+            api = InstaApiBuilder.CreateBuilder()
+                .SetUser(user).UseLogger(new DebugLogger(LogLevel.Exceptions))
+                .Build();
+
+            var logInRequest = await api.LoginAsync();
+            if (logInRequest.Succeeded)
+                Console.WriteLine("Logged In!");
+            else
+                Console.WriteLine("Error Logging In!\n" + logInRequest.Info.Message);
         }
     }
 }
